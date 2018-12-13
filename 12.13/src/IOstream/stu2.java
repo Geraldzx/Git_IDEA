7b44b0fe88910a3e582030063c97e55081c0041e
Geraldzx <45187348+Geraldzx@users.noreply.github.com>
2018-12-13 21:19:59 +0800

    IO流（错误）

---
+++ 12.13/src/IOstream/stu2.java
@@ -0,0 +1,53 @@
+package IOstream;
+
+import java.io.Serializable;
+
+class Stu2 implements Serializable {
+
+    private static final long serialVersionUID = -5500926321897468779L;
+    private String name;
+    private int age;
+    private char sex;
+
+    @Override
+    public String toString() {
+        return "Stu{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public Stu2() {
+    }
+
+    public Stu2(String name, int age, char sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+}
