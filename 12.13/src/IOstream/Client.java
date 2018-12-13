84f1c5f977cbd7552a09a3742b584f91d209f829
Geraldzx <45187348+Geraldzx@users.noreply.github.com>
2018-12-13 21:20:49 +0800

    IO流

--- 12.13/src/IOstream/Client.java
+++ 12.13/src/IOstream/Client.java
@@ -1,9 +1,6 @@
 package IOstream;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,7 +10,7 @@ public class Client {
         stu.add(new Stu("张三",18,'女'));
         stu.add(new Stu("李四",28,'男'));
 
-        File file = new File("f://student");
+        File file = new File("student");
         if(!file.exists() && file.isFile()==false) {
             try {
                 file.createNewFile();
@@ -22,9 +19,38 @@ public class Client {
             }
         }
 
+        //写入文件
+        in(stu, file);
+        //读取文件
+        read(file);
+    }
+
+    private static void in(List<Stu> stu, File file) {
         try {
-            FileInputStream input = new FileInputStream((File) stu);
+            OutputStream out = new FileOutputStream(file);
+            for (Stu stu1 : stu) {
+                String s = stu1.toString()+"\n";
+                byte b [] = s.getBytes();
+                out.write(b);
+            }
+            out.close();
         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }  catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void read(File file) {
+        try {
+            InputStream is = new FileInputStream(file);
+            int filelength = (int) file.length();
+            byte b[]=new byte[filelength];
+            is.read(b);
+            System.out.println(new String(b));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
